1b9b30016f9d197300c0b55fc520ae8178b9f3fd
Pedro Pugas <pedropugas@outlook.com.br>
2022-07-21 14:41:13 -0300

    requisito 2

--- modulo-1/bloco-4/dia-1/script.js
+++ modulo-1/bloco-4/dia-1/script.js
@@ -1,5 +1,5 @@
-const a = 10;
-const b = 20;
+const a = 20;
+const b = 30;
 
 var soma = a + b;
 var subtracao = a - b;
@@ -7,3 +7,12 @@ var multiplicacao = a * b;
 var divisao = a / b;
 var modulo = a % b;
 
+if (a > b) {
+    console.log(`O número ${a} é maior do que ${b}`);
+
+} else if (a === b) {
+    console.log(`O número ${a} é igual a ${b}`);
+
+}   else {
+    console.log(`O número ${a} é menor do que ${b}`);
+} 
