d197dfc3f6450d04d0be03c7575fae003166844a
Pedro Pugas <pedropugas@outlook.com.br>
2022-08-05 11:19:20 -0300

    Requisito 3

---
+++ modulo-1/bloco-6/dia-1/script.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector('#submit-button');
+// const form = document.querySelector
+
+function btnPrevent(event) {
+    event.preventDefault();
+}
+
+botao.addEventListener('click', btnPrevent);
+
+//
+
