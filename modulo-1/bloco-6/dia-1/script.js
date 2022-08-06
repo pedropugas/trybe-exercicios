19d18e160a266834f2d36e4372f6ab9ccfb2df77
Pedro Pugas <pedropugas@outlook.com.br>
2022-08-06 19:05:25 -0300

    Bônus

--- modulo-1/bloco-6/dia-1/script.js
+++ modulo-1/bloco-6/dia-1/script.js
@@ -1,12 +1,13 @@
 const botao = document.querySelector('#submit-button');
 const form = document.querySelector('#my-form');
 const botaoRst = document.querySelector('#reset-button')
+const nome = document.querySelector('#nome-completo')
 
-function btnPrevent(event) {
-    event.preventDefault();
-}
+// function btnPrevent(event) {
+//     event.preventDefault();
+// }
 
-botao.addEventListener('click', btnPrevent);
+// botao.addEventListener('click', btnPrevent);
 
 //
 
@@ -15,3 +16,16 @@ function rstBtn() {
 }
 
 botaoRst.addEventListener('click', rstBtn)
+
+//
+
+// function caracteresCheck(name) {
+//     let char = name.value.trim();
+//     if(char.length < 10 || char.length > 40) {
+//         alert('Dados Inválidos')
+//     } else {
+//         alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+//     }
+// }
+
+// nome.addEventListener('change', caracteresCheck)
